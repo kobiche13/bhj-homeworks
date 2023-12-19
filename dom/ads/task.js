c2f3cb084d885bb76a677ddfc7246d6ac4663f60
Artem Serebriakov <koba156@yandex.ru>
2023-12-20 00:09:25 +0300

    Задание про ротатор исправил

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -1,17 +1,20 @@
-const rotator = Array.from(document.querySelectorAll('.rotator__case'));
-i = 0;
-   
-setInterval(() =>{  //не знаю как учесть что их несколько на странице    
-    if (i !== rotator.length-1){
-        rotator[i+1].classList.add('rotator__case_active');
-        rotator[i].classList.remove('rotator__case_active');
-        i = (i+1) % rotator.length; 
-        
-    } else {
-        rotator[i].classList.remove('rotator__case_active');
-        i = (i+1) % rotator.length; 
-        rotator[i].classList.add('rotator__case_active');
-    } 
-}, 1000)
+
+const rotator = Array.from(document.querySelectorAll('.rotator'));
+
+for (let  j = 0; j < rotator.length; j++){
+    let i = 0;
+    setInterval(() =>{          
+        if (i !== rotator[j].children.length - 1){
+            rotator[j].children[i].classList.remove('rotator__case_active');
+            rotator[j].children[i+1].classList.add('rotator__case_active');            
+            i = (i+1) % rotator[j].children.length;           
+        } else {
+            rotator[j].children[i].classList.remove('rotator__case_active');
+            i = (i+1) % rotator[j].children.length; 
+            rotator[j].children[i].classList.add('rotator__case_active');
+        } 
+    }, 1000)
+
+}
 
    
